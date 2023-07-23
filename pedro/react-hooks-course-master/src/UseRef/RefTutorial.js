b219a7887cd018ee5273d7e12510d4ce6d11d633
vishnu s <vishnusudeesh390@gmail.com>
2023-07-23 23:48:25 +0530

    pedro

--- pedro/react-hooks-course-master/src/UseRef/RefTutorial.js
+++ pedro/react-hooks-course-master/src/UseRef/RefTutorial.js
@@ -1,27 +1,18 @@
-import {  useRef,} from 'react'
-import './App.css'
+import React, { useRef } from "react";
 
-
-function App() {
+function RefTutorial() {
   const inputRef = useRef(null);
 
   const onClick = () => {
-    inputRef.current.focus = "";
+    inputRef.current.value = "";
   };
-
   return (
-    <>
-     <div>
+    <div>
       <h1>Pedro</h1>
       <input type="text" placeholder="Ex..." ref={inputRef} />
       <button onClick={onClick}>Change Name</button>
     </div>
-  
-
-
-  
-    </>
-  )
+  );
 }
 
-export default App
+export default RefTutorial;
